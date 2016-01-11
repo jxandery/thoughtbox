4a799db84c82bc055f1b3e51b9ef227a9f2f43ac
Jack Yeh <jack.yeh@gmail.com>
2016-01-11 15:00:38 -0700

    bug fix: update the routes and helper path names to reflect api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'sessions/new'
 
   resources :users, only: [:new, :create, :show]
-  resources :links, only: [:index]
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
