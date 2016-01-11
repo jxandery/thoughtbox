30b6fe0ce6308c0788e8da775c084f7116f89e1c
Jack Yeh <jack.yeh@gmail.com>
2016-01-11 15:50:42 -0700

    setup: add poltergeist gem; configure poltergeist in test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,8 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/pride'
 require 'capybara/rails'
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist
 
 class ActionController::TestCase
   def json_response
@@ -20,6 +22,14 @@ end
 class ActionDispatch::IntegrationTest
   include Capybara::DSL
   include Rails.application.routes.url_helpers
+
+  def use_javascript
+    Capybara.current_driver = Capybara.javascript_driver
+  end
+
+  def reset_driver
+    Capybara.current_driver = nil
+  end
 
   def teardown
     reset_session!
