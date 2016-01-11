d2cf3daccf9d0f8d2b352c93ac6062cb7d69f59a
Jack Yeh <jack.yeh@gmail.com>
2016-01-11 14:07:03 -0700

    create/pass test: #index returns the correct number of links

--- test/controllers/api/v1/links_controller_test.rb
+++ test/controllers/api/v1/links_controller_test.rb
@@ -12,4 +12,10 @@ class Api::V1::LinksControllerTest < ActionController::TestCase
 
     assert_kind_of Array, json_response
   end
+
+  test '#index returns the correct number of links' do
+    get :index, format: :json
+
+    assert_equal Link.count, json_response.count
+  end
 end
