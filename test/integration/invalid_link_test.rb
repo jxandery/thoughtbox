22760c2d090b715ee3d6ad23f3e739644800d578
Jack Yeh <jack.yeh@gmail.com>
2016-01-11 13:23:03 -0700

    create test: invalid link submission will result in error message

---
+++ test/integration/invalid_link_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class InvalidLinkTest < ActionDispatch::IntegrationTest
+
+  test "invalid link submission will result in error message" do
+    user = User.create(email_address: "jack", password: "password")
+
+    visit login_path
+    fill_in "Email address", with: user.email_address
+    fill_in "Password", with: "password"
+    click_button "Login"
+
+    assert page.has_content?("Welcome, jack")
+    assert_current_path(links_path)
+
+    fill_in "Title", with: 'link title'
+    fill_in "URL", with: "http://cnn.com"
+    click_button "Add Thought"
+
+    assert page.has_content?("link title")
+    assert page.has_content?("http://cnn.com")
+    assert_current_path(links_path)
+  end
+end
