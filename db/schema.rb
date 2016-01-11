55a461c9f9859c947cad69f13ba47d6b7af219ff
Jack Yeh <jack.yeh@gmail.com>
2016-01-11 13:01:21 -0700

    create links model with title, url and read status (default: false)

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160111180601) do
+ActiveRecord::Schema.define(version: 20160111195815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "links", force: :cascade do |t|
+    t.string   "url"
+    t.string   "title"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "read",       default: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email_address"
