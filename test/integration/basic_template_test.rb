8bb7e93db3ba6d6c48038b5f500acab087e50a17
Jack Yeh <jack.yeh@gmail.com>
2016-01-11 15:45:22 -0700

    create/pass tests: adding the form and links container - it has an links container on the page - it has a form for creating new links - form has an text input for a new link title - form has an text input for a new link button - form has an input button

--- test/integration/basic_template_test.rb
+++ test/integration/basic_template_test.rb
@@ -11,4 +11,30 @@ class BasicTemplateTest < ActionDispatch::IntegrationTest
     visit root_path
     assert page.find("h1").has_content? "Thought Box"
   end
+
+  test "it has an links container on the page" do
+    visit root_path
+    assert page.has_css? ".links"
+  end
+
+  test "it has a form for creating new links" do
+    visit root_path
+    assert page.has_css? "form.new-link"
+  end
+
+  test "form has an text input for a new link title" do
+    visit root_path
+    assert page.has_css? "form.new-link input[type='text'].new-link-title"
+  end
+
+  test "form has an text input for a new link button" do
+    visit root_path
+    assert page.has_css? "form.new-link input[type='text'].new-link-body"
+  end
+
+  test "form has an input button" do
+    visit root_path
+    save_and_open_page
+    assert page.has_css? "form.new-link input[type='submit'].new-link-submit"
+  end
 end
