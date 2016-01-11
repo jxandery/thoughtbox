9c91b9711bbc80320ee98a4155f6461d92484146
Jack Yeh <jack.yeh@gmail.com>
2016-01-11 13:09:38 -0700

    create/pass test: it should be invalid without a title

--- test/models/link_test.rb
+++ test/models/link_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class LinkTest < ActiveSupport::TestCase
-  test 'it should be invalid without a title or url' do
+  test 'it should be invalid without a title and url' do
     link_without_title_or_url = Link.new
     refute(link_without_title_or_url.valid?)
+  end
+
+  test 'it should be invalid without a title' do
+    link = Link.new(url: 'body')
+    refute(link.valid?)
   end
 end
