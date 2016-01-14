bc5e301df06b0658816ecaf1b87e0caefbf13627
Jack Yeh <jack.yeh@gmail.com>
2016-01-14 15:15:13 -0700

    refactor: remove extraneous code

--- app/assets/javascripts/links.js
+++ app/assets/javascripts/links.js
@@ -3,9 +3,4 @@ function Link(data) {
   this.title  = data.title;
   this.url    = data.url;
   this.read   = data.read;
-  this.readStatus = this.viewed();
-}
-
-Link.prototype.viewed = function() {
-  return this.read ? "read" : "unread"
 }
