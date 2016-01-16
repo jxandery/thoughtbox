e26ef25f8d12bca1117386ce233a47e7946f075f
Jack Yeh <jack.yeh@gmail.com>
2016-01-16 15:01:26 -0700

    add functionality: user can edit links without page refresh - create addEditLinkForm function - create updateLink function - create editLink function - add editLink() call to document ready - create updateLinkRecord function that includes ajax request

--- app/assets/javascripts/render_links.js
+++ app/assets/javascripts/render_links.js
@@ -8,8 +8,26 @@ $(document).ready(function () {
     .then(prependLinksToContainer);
   updateReadStatus();
   updateUnreadStatus();
-
+  editLink();
 });
+
+var editForm =  "<div class='edit-form'><div class='row'><div class='col-sm-4'><h6>Edit Title</h6>"
++ "<input class='form-control' type='text' id='edit-title'></div></div><div class='row'><div class='col-sm-8'>"
++ "<h6>Edit URL</h6><input class='form-control' type='text' id='edit-url'></div></div><input "
++ "class='btn btn-default pull-right update-link' type='button' name='submit' value='Update Link'></div></div>";
+
+function addEditLinkForm (link) {
+  $('div').remove('.edit-form');
+  link.append(editForm);
+  updateLinkRecord(link);
+}
+
+function updateLink(result, link) {
+  $('div').remove('.edit-form');
+  link.find('h2').replaceWith( "<h2>" + result.title + "</h2>" );
+  link.find('p').replaceWith( "<p>" + result.url + "</p>" );
+}
+
 
 function prependLinkToContainer(link, textStatus, jqXHR) {
   $(linkTemplate(link)).prependTo(linksContainer);
@@ -72,6 +90,44 @@ function updateUnreadStatus() {
     var read            = linkHTML.hasClass('read-true');
 
     if (read) { updateReadStatusRecord(linkHTML, linkId, false) };
+  });
+}
+
+function editLink() {
+  $(document).on('click', '.link-edit', function(event){
+    var link            = $(this).closest('.link');
+
+    addEditLinkForm(link);
+  });
+}
+
+function updateLinkRecord(link) {
+  // when update form is clicked send an ajax request
+  // try a post request first and then a patch request using serialize
+  // on the success callback remove the form and return the result
+  //
+  $(document).on('click', '.update-link', function(event){
+    event.preventDefault();
+    var $link            = $(this).closest('.link');
+      //var $linkId         = $(this).closest('.link').attr('data-id');
+      var $updatedTitle    = $('#edit-title').val();
+    var $updatedUrl      = $('#edit-url').val();
+    var $updatedParams   = {
+      link: {
+        title: $updatedTitle,
+        url:   $updatedUrl
+      }
+    };
+
+    $.ajax({
+      method:       'PATCH',
+      url:          '/api/v1/links/' + $link.attr('data-id'),
+      data:         $updatedParams,
+      success:      updateLink($updatedParams.link, $link),
+      error:        function(request, errorType, errorMessage) {
+        console.log('Error: ' + errorType + ' with message: ' + errorMessage);
+      }
+    });
   });
 }
 
