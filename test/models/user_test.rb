dd30ff9d27785fa1e5556dce8082bcf7723544b1
Jack Yeh <jack.yeh@gmail.com>
2016-01-17 20:59:17 -0700

    pass test: it should be invalid without an email and password

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,24 @@
-require 'test_helper'
+#require 'test_helper'
+require './test/test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'it should be invalid without an email and password' do
+    user_without_email_or_password = User.new
+    refute(user_without_email_or_password.valid?)
+  end
+
+  #test 'it should be invalid without a title' do
+    #link = Link.new(url: 'http://urlgalore.com')
+    #refute(link.valid?)
+  #end
+
+  #test 'it should be invalid without a url' do
+    #link = Link.new(title: 'title')
+    #refute(link.valid?)
+  #end
+
+  #test 'it should be invalid without a valid url' do
+    #link = Link.new(title: 'title', url: 'asdfpoij')
+    #refute(link.valid?)
+  #end
 end
