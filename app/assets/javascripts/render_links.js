097e0e39c8f4b422722c423a9509cb123a300871
Jack Yeh <jack.yeh@gmail.com>
2016-01-17 19:30:59 -0700

    add functionality: user can click on ABC sort button and sort links alphabetically without page reload

--- app/assets/javascripts/render_links.js
+++ app/assets/javascripts/render_links.js
@@ -12,6 +12,7 @@ $(document).ready(function () {
   searchLinks();
   sortRead();
   sortUnread();
+  sortABC();
 });
 
 function sortRead() {
@@ -25,6 +26,22 @@ function sortUnread() {
   $(document).on('click', '.unread-link', function(event){
     $('.read-false').show();
     $('.read-true').hide();
+  });
+}
+
+function sortABC(){
+  $('.sort-abc-link').on('click', function() {
+    var $link = $('.link');
+
+    $link.sort(function(one, two) {
+      var first = $(one).find('.link-title').text().toLowerCase();
+      var second = $(two).find('.link-title').text().toLowerCase();
+      return (first < second) ? 1 : 0;
+    });
+
+    $.each($link, function(index, element) {
+      $('.links').prepend(element);
+    });
   });
 }
 
